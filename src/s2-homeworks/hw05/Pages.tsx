b1e9bb9853e5337f6526f66dce94d2bebcb27761
sli39 <sliva391985@gmail.com>
2025-11-29 12:40:32 +0300

    initial commit

---
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Navigate, Route, Routes} from 'react-router-dom'
+import Error404 from './pages/Error404'
+import Junior from './pages/Junior'
+import JuniorPlus from './pages/JuniorPlus'
+import PreJunior from './pages/PreJunior'
+
+export const PATH = {
+    PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    JUNIOR_PLUS: '/junior-plus',
+}
+
+function Pages() {
+    return (
+        <div>
+            {/*Routes выбирает первый подходящий роут*/}
+            <Routes>
+                {/*роутинг будут писать студенты*/}
+                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+                {/*<Route ...*/}
+                <Route path='/' element={<Navigate to={PATH.PRE_JUNIOR}/> }></Route>
+
+
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}></Route>
+                <Route path={PATH.JUNIOR} element={<Junior/>}></Route>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}></Route>
+
+                {/*роуты для /pre-junior, /junior, /junior-plus*/}
+                {/*<Route ...*/}
+                {/*<Route ...*/}
+                {/*<Route ...*/}
+
+                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+                {/*<Route ...*/}
+                <Route path="*" element={<Error404/>}></Route>
+            </Routes>
+        </div>
+    )
+}
+
+export default Pages
